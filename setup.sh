9112dce0f2344d568eccda54371007e994904a1c
LYSYyy <33262838+LYSYyy@users.noreply.github.com>
2018-04-15 05:24:14 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-# SETUP SCRIPT by LYSYyy v1.0
+# SETUP SCRIPT by LYSYyy v1.1
 
+function main
+{
 	echo
 	echo -e " \e[1;32m[+]\e[1;39m FIRST CONFIGURE...\e[0m"
 	echo
 	echo -ne "Choose language (PL / EN):\e[1m "
 	read -e lang
 	echo
-	if [ $lang = PL ]; then
+	if [ $lang = "PL" ]; then
 		echo -e "\e[21;90m"
 		rm /bin/lsploit
 		cp ./lsploit/PL/* /bin
@@ -16,7 +18,23 @@
 		echo -e "\e[0m"
 		lsploit
 		
-	elif [ $lang = EN ]; then
+	elif [ $lang = "pl" ]; then
+		echo -e "\e[21;90m"
+		rm /bin/lsploit
+		cp ./lsploit/PL/* /bin
+		chmod 755 /bin/lsploit
+		echo -e "\e[0m"
+		lsploit
+		
+	elif [ $lang = "EN" ]; then
+		echo -e "\e[21;90m"
+		rm /bin/lsploit
+		cp ./lsploit/EN/* /bin
+		chmod 755 /bin/lsploit
+		echo -e "\e[0m"
+		lsploit
+		
+	elif [ $lang = "en" ]; then
 		echo -e "\e[21;90m"
 		rm /bin/lsploit
 		cp ./lsploit/EN/* /bin
@@ -25,7 +43,8 @@
 		lsploit
 		
 	else
-		echo -e "\e[1;31mError\e[0m"
-		exit
+		main
 	fi
-	exit
+}
+
+main
